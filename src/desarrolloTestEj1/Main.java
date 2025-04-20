210a04811591cb111d14cea4938974c9091496b3
rottensyst88 <arib042@yahoo.com>
2025-04-20 00:11:01 -0400

    Add Main class with Ceros method for counting zeros in binary representation ~AB

---
+++ src/desarrolloTestEj1/Main.java
@@ -0,0 +1,24 @@
+package desarrolloTestEj1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Ceros(0b01010100010101000101010001010100, (byte) 10));
+    }
+
+    public static byte Ceros(int x, byte p){
+        int mask = 0x80000000;
+        int i = 0;
+        int cont = 0;
+
+        while(i < p){
+            if((x & mask) == mask){
+                cont++;
+            }
+            mask = mask >>> 1;
+            i++;
+        }
+
+        return (byte) (p - cont);
+
+    }
+}

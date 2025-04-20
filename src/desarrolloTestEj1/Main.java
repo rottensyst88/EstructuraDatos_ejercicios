6d745594776cde5bb540f2511a5d602e069cc022
rottensyst88 <arib042@yahoo.com>
2025-04-20 01:09:06 -0400

    Add Lista class with nested Nodo class and initial SubLista method implementation ~AB

--- src/desarrolloTestEj1/Main.java
+++ src/desarrolloTestEj1/Main.java
@@ -19,6 +19,5 @@ public class Main {
         }
 
         return (byte) (p - cont);
-
     }
 }
---
+++ src/desarrolloTestEj2/Lista.java
@@ -0,0 +1,52 @@
+package desarrolloTestEj2;
+
+public class Lista {
+
+    public class Nodo {
+        int objeto;
+        Nodo siguiente;
+
+        Nodo(int i, Nodo s) {
+            objeto = i;
+            siguiente = s;
+        }
+    }
+
+    Nodo head;
+
+    public Lista() {
+        head = null;
+    }
+
+    public static boolean SubLista(Lista l1, Lista l2) {
+        /*
+
+        Nodo ls2 = l2.head;
+
+        while (ls2 != null) {
+            Nodo temp2 = ls2;
+            Nodo ls1 = l1.head;
+            while (ls1 != null && temp2.objeto == ls1.objeto) {
+                temp2 = temp2.siguiente;
+                ls1 = ls1.siguiente;
+            }
+            if (ls1 == null) {
+                return true; // Se encontró la sublista
+            }
+            ls2 = ls2.siguiente;
+        }
+
+        return false; // No se encontró la sublista
+
+         */
+        Nodo z = null;
+        for(Nodo x = l2.head; x.siguiente != null; x=x.siguiente ) {
+            if(x.objeto == l1.head.objeto) {
+                z = x;
+                break;
+            }
+        }
+
+        while () {}
+    }
+}
